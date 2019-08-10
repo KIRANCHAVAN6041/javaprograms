0bcf7a23569e5a657ad4932cc65f440fd4d0ac9a
slot2 <kiran.chavan6041@gmail.com>
2019-08-10 16:57:01 +0530

    first commit

---
+++ src/com/bridgelabz/utility/Utility.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.utility;
+
+public class Utility {
+	public void add(int a, int b) {
+		
+	int c=a+b;
+	System.out.println(c);
+	}
+
+}
---
+++ src/com/brigdelabz/function/Additon.java
@@ -0,0 +1,17 @@
+package com.brigdelabz.function;
+
+
+import com.bridgelabz.utility.Utility;
+
+public class Additon {
+
+	public static void main(String[] args) {
+		
+		int a=11;
+		int b=12;
+		Utility  utility =new Utility();
+		utility.add(a, b);
+			
+	}
+
+}
